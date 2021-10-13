6735ee2d98d113dc233ed8369dfc653add22bdb7
Ezequiel Ramirez <ezequielram@gmail.com>
2021-10-13 12:25:42 -0300

    ajuste final

--- src/context/PokeContext.jsx
+++ src/context/PokeContext.jsx
@@ -30,26 +30,26 @@ const PokeProvider = ({ children }) => {
     const handleChange = e => {
         setText(e.target.value.toLowerCase());
         filtrar(e.target.value);
-      
-    }
-const filtrar = useMemo(() => 
-(terminoBusqueda) => {
-    var resultadosBusqueda = pokeSearch.filter((elemento) => {
-            if (elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
-                || elemento.id.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())) {
-                return elemento;
-            }
-        });
-    setPokeItem(resultadosBusqueda);
-    
-}, [pokeSearch])
-    
-/* volver y resetear estados */
-const handleReturn =()=>{
-    setSearchBar(false);
-    getPoke()
 
-}
+    }
+    const filtrar = useMemo(() =>
+        (terminoBusqueda) => {
+            var resultadosBusqueda = pokeSearch.filter((elemento) => {
+                if (elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+                    || elemento.id.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())) {
+                    return elemento;
+                }
+            });
+            setPokeItem(resultadosBusqueda);
+
+        }, [pokeSearch])
+
+    /* volver y resetear estados */
+    const handleReturn = () => {
+        setSearchBar(false);
+        getPoke()
+
+    }
     /* Busqueda por id o nombre en boton buscar */
     const getItem = async (id) => {
         if (validarCampos) {
@@ -79,87 +79,87 @@ const handleReturn =()=>{
         }
 
     }
-/* listado inicial de Pokemones */
-const getPoke = useMemo(() => 
-async () => {
-    try {
-        const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'";
-        const res = await fetch(url);
-        const json = await res.json();
-        setPrev(json.previous);
-        setNext(json.next);
-        setPokeItem([]);
-        /* setPokeSearch([]); */
-        for (let i = 0; i < json.results.length; i++) {
+    /* listado inicial de Pokemones */
+    const getPoke = useMemo(() =>
+        async () => {
             try {
-                const respuesta = await fetch(json.results[i].url);
-                const pokemon = await respuesta.json()
-                const pokemonCard = {
-                    name: pokemon?.name,
-                    id: pokemon?.id,
-                    img: pokemon?.sprites?.front_default,
-                    imgDetail: pokemon?.sprites?.other["official-artwork"]?.front_default,
-                    height: pokemon?.height,
-                    weight: pokemon?.weight,
-                    type: pokemon?.types[0]?.type?.name,
-                    ability: pokemon?.abilities[1]?.ability?.name
+                const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'";
+                const res = await fetch(url);
+                const json = await res.json();
+                setPrev(json.previous);
+                setNext(json.next);
+                setPokeItem([]);
+                /* setPokeSearch([]); */
+                for (let i = 0; i < json.results.length; i++) {
+                    try {
+                        const respuesta = await fetch(json.results[i].url);
+                        const pokemon = await respuesta.json()
+                        const pokemonCard = {
+                            name: pokemon?.name,
+                            id: pokemon?.id,
+                            img: pokemon?.sprites?.front_default,
+                            imgDetail: pokemon?.sprites?.other["official-artwork"]?.front_default,
+                            height: pokemon?.height,
+                            weight: pokemon?.weight,
+                            type: pokemon?.types[0]?.type?.name,
+                            ability: pokemon?.abilities[1]?.ability?.name
+                        }
+                        setPokeItem(prevArray => [...prevArray, pokemonCard])
+                        /* setPokeSearch(prevArray => [...prevArray, pokemonCard]) */
+                        setStateSpinner(false)
+                        setSearchBar(false)
+                    } catch (err) {
+                        setError(err || "Ocurrió un error")
+                        console.log(err);
+                    }
                 }
-                setPokeItem(prevArray => [...prevArray, pokemonCard])
-                /* setPokeSearch(prevArray => [...prevArray, pokemonCard]) */
-                setStateSpinner(false)
-                setSearchBar(false)
             } catch (err) {
                 setError(err || "Ocurrió un error")
                 console.log(err);
+                alert(err)
             }
         }
-    } catch (err) {
-        setError(err || "Ocurrió un error")
-        console.log(err);
-        alert(err)
-    }
-}
-, [])
-    
-   
-   
-   
-/* listado total de Pokemones */
-const getPokeTotal = useMemo (() =>
-async () => {
+        , [])
 
-    try {
-        const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118";
-        const res = await fetch(url);
-        const json = await res.json();
-        setPokeSearch([]);
-        for (let i = 0; i < json.results.length; i++) {
+
+
+
+    /* listado total de Pokemones */
+    const getPokeTotal = useMemo(() =>
+        async () => {
+
             try {
-                const respuesta = await fetch(json.results[i].url);
-                const pokemon = await respuesta.json()
-                const pokemonCard = {
-                    name: pokemon?.name,
-                    id: pokemon?.id,
-                    img: pokemon?.sprites?.front_default,
-                    imgDetail: pokemon?.sprites?.other["official-artwork"]?.front_default,
-                    height: pokemon?.height,
-                    weight: pokemon?.weight,
-                    type: pokemon?.types[0]?.type?.name,
-                    ability: pokemon?.abilities[1]?.ability?.name
+                const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118";
+                const res = await fetch(url);
+                const json = await res.json();
+                setPokeSearch([]);
+                for (let i = 0; i < json.results.length; i++) {
+                    try {
+                        const respuesta = await fetch(json.results[i].url);
+                        const pokemon = await respuesta.json()
+                        const pokemonCard = {
+                            name: pokemon?.name,
+                            id: pokemon?.id,
+                            img: pokemon?.sprites?.front_default,
+                            imgDetail: pokemon?.sprites?.other["official-artwork"]?.front_default,
+                            height: pokemon?.height,
+                            weight: pokemon?.weight,
+                            type: pokemon?.types[0]?.type?.name,
+                            ability: pokemon?.abilities[1]?.ability?.name
+                        }
+                        setPokeSearch(prevArray => [...prevArray, pokemonCard])
+                    } catch (err) {
+                        setError(err || "Ocurrió un error")
+                        console.log(err);
+                    }
                 }
-                setPokeSearch(prevArray => [...prevArray, pokemonCard])
             } catch (err) {
                 setError(err || "Ocurrió un error")
                 console.log(err);
+                alert(err)
             }
         }
-    } catch (err) {
-        setError(err || "Ocurrió un error")
-        console.log(err);
-        alert(err)
-    }
-}
-,[pokeSearch])
+        , [pokeSearch])
 
 
     /* paginacion */
